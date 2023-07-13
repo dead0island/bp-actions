8859a63efd884efc3e045bf5f1135d4d5f6af948
Gong Bing <bingbing@ahnu.edu.cn>
2023-07-13 21:51:36 +0800

    Update Alist&Cpolar.sh

--- Alist&Cpolar.sh
+++ Alist&Cpolar.sh
@@ -1,5 +1,6 @@
 echo "安装Alist中......"
 wget https://alist.nn.ci/v3.sh
+sudo chmod 777 ./v3.sh
 ./v3.sh install
 sed -i 's#80#8080#g' /opt/alist/data/config.json
 systemctl enable alist
@@ -7,7 +8,9 @@ systemctl start alist
 echo "Alist安装成功！"
 echo "..............................................................."
 echo "安装cpolar......"
-curl -L https://www.cpolar.com/static/downloads/install-release-cpolar.sh | sudo bash
+wget https://www.cpolar.com/static/downloads/install-release-cpolar.sh
+sudo chmod 777 ./install-release-cpolar.sh
+./install-release-cpolar.sh
 cpolar version
 cpolar authtoken ZjFmOWQ4OTMtMDljMS00MTY4LTg3MWEtNDk2OTQ5ZTQyMDJm
 sudo systemctl enable cpolar
